ac703a78cd75f1feba0a183e846f8316d0deab48
Fabio.Vitalba <fabvit9@gmail.com>
2024-12-03 00:18:27 +0100

    Improve Composable view

--- app/src/main/java/com/unibz/songplayer/MainActivity.kt
+++ app/src/main/java/com/unibz/songplayer/MainActivity.kt
@@ -8,15 +8,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -59,8 +66,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PlaySong(song: Song, modifier: Modifier = Modifier) {
-    Box(modifier = Modifier.background(brush = gradientBrush)) {
-        Column {
+    var songId by remember { mutableStateOf(0) } // state value
+    Box(modifier = Modifier
+        .background(brush = gradientBrush)
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    ) {
+        Column( horizontalAlignment = Alignment.CenterHorizontally ) {
             Image(
                 painter = painterResource(song.darwableResource),
                 contentDescription = "sunrise image",
@@ -88,6 +100,17 @@ fun PlaySong(song: Song, modifier: Modifier = Modifier) {
                     .align(Alignment.CenterHorizontally),
                 fontSize = 6.sp
             )
+            Row {
+                Button(onClick = { /* TODO */ }) {
+                    Text("PREV"/* stringResource(R.string.next) */)
+                }
+                Button(onClick = { /* TODO */ }) {
+                    Text("PLAY/PAUSE"/* stringResource(R.string.next) */)
+                }
+                Button(onClick = { /* TODO */ }) {
+                    Text("NEXT"/* stringResource(R.string.next) */)
+                }
+            }
         }
     }
 }
