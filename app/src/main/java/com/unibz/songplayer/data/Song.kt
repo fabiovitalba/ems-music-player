d1afbf1bd380ce7e683803ec46feb55ed841ba6c
Fabio.Vitalba <fabvit9@gmail.com>
2024-12-03 21:19:07 +0100

    Added Song Duration

--- app/src/main/java/com/unibz/songplayer/data/Song.kt
+++ app/src/main/java/com/unibz/songplayer/data/Song.kt
@@ -1,11 +1,4 @@
 package com.unibz.songplayer.data
 
-data class Song(val title: String, val artist: String) {
-    //fun play() {
-    //    println("Playing '$title' by '$artist'.");
-    //}
-    //
-    //fun stop() {
-    //    println("Stop playing song '$title'.");
-    //}
+data class Song(val title: String, val artist: String, val duration: Int) {
 }
